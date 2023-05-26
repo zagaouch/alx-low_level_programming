6e39a92dc231fc3aafcce77e86d4cbfc381a7dee
ilyas zagaouch <i.zagaouch@gmail.com>
2023-05-26 01:06:22 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,6 +0,0 @@
-#ifndef PI_L
-#define PI_L
-
-#define PI 3.14159265359
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUN_LIKE_MACRO
-#define FUN_LIKE_MACRO
-
-#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
-
-#endif
