f38baefd89ba3c94e4288398fa8e4352331ff6f3
zagaouch <i.zagaouch@gmail.com>
2023-07-07 04:31:37 -0700

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * binary_check - function that check if string 0 or 1
+ * @b: pointer to astring of 0 or 1
+ * Return: 0 or 1
+ */
+int binary_check(const char *b)
+{
+	int i = 0;
+	while (b =! '\0')
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number,
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i = 0;
+
+	if (b == NULL || !binary_check(b))
+		return (0);
+	while (b[i] != '\0')
+	{
+		result = result << 1;
+		if (b[i] == '1')
+			result |= 1;
+		i++;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+unsigned int binary_to_uint(const char *b);
+
+
+#include <stdio.h>
+#endif
