c02fa7ce8876454f6862df54608c8df9755ea392
zagaouch <i.zagaouch@gmail.com>
2024-02-06 21:54:50 +0100

    auto commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,17 +8,18 @@
  * @value : the value to search
  * Return: array or if array is NULL, your function must return -1
 */
-int linear_search(int *array, size_t size, int value){
-    size_t i;
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
 
-    if (array == NULL)
-        return (-1);
+if (array == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
 }
